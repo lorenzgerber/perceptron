545d490bd0e6ff987b81a9c98b2d26ee1a122c3b
Lorenz Gerber <lorenz.gerber@lablicate.com>
2019-10-10 22:13:17 +0200

    initial checkin

---
+++ src/main.py
@@ -0,0 +1,16 @@
+
+
+
+# Parsing and checking cli args
+
+
+# reading data
+
+
+# divide data
+
+
+# train perceptron
+
+
+# validate perceptron
---
+++ src/perceptron/perceptron.py
@@ -0,0 +1,8 @@
+
+
+
+class Perceptron:
+
+    def __init__(self, nooinputs, noonodes ):
+        print("I m a perceptron")
+        
